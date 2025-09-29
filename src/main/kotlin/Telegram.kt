5a794aa7ff8a42be8bb088e1cec816846090517d
lion88 <lion_alex1988@mail.ru>
2025-09-29 23:25:16 +0300

    KTB-12 obtain a Telegram bot token and perform basic HTTP requests

---
+++ src/main/kotlin/Telegram.kt
@@ -0,0 +1,19 @@
+import java.net.URI
+import java.net.http.HttpClient
+import java.net.http.HttpRequest
+import java.net.http.HttpResponse
+
+fun main(args: Array<String>) {
+
+    val botToken = args[0]
+    val urlGetMe = "https://api.telegram.org/bot$botToken/getMe"
+    val urlGetUpdates = "https://api.telegram.org/bot$botToken/getUpdates"
+    val client = HttpClient.newBuilder().build()
+
+    val request1 = HttpRequest.newBuilder().uri(URI.create(urlGetMe)).build()
+    val request2 = HttpRequest.newBuilder().uri(URI.create(urlGetUpdates)).build()
+    val response1: HttpResponse<String> = client.send(request1, HttpResponse.BodyHandlers.ofString())
+    val response2: HttpResponse<String> = client.send(request2, HttpResponse.BodyHandlers.ofString())
+    println(response1.body())
+    println(response2.body())
+}
