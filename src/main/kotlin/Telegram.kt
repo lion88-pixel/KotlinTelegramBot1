556032bb989087999e1e793fd850e59486e25916
lion88 <lion_alex1988@mail.ru>
2025-10-02 23:42:12 +0300

    KTB-13 send a request with parameters and parse the response

--- src/main/kotlin/Telegram.kt
+++ src/main/kotlin/Telegram.kt
@@ -6,14 +6,24 @@ import java.net.http.HttpResponse
 fun main(args: Array<String>) {
 
     val botToken = args[0]
-    val urlGetMe = "https://api.telegram.org/bot$botToken/getMe"
-    val urlGetUpdates = "https://api.telegram.org/bot$botToken/getUpdates"
-    val client = HttpClient.newBuilder().build()
+    var updateId = 0
+    while (true) {
+        Thread.sleep(2000)
+        val updates: String = getUpdates(botToken, updateId)
+        println(updates)
 
-    val request1 = HttpRequest.newBuilder().uri(URI.create(urlGetMe)).build()
-    val request2 = HttpRequest.newBuilder().uri(URI.create(urlGetUpdates)).build()
-    val response1: HttpResponse<String> = client.send(request1, HttpResponse.BodyHandlers.ofString())
-    val response2: HttpResponse<String> = client.send(request2, HttpResponse.BodyHandlers.ofString())
-    println(response1.body())
-    println(response2.body())
+        val startUpdateId = updates.lastIndexOf("update_id")
+        val endUpdateId = updates.lastIndexOf(",\n\"message\"")
+        if (startUpdateId == -1 || endUpdateId == -1) continue
+        val updateIdString = updates.substring(startUpdateId + 11, endUpdateId)
+        updateId = updateIdString.toInt() + 1
+    }
+}
+
+fun getUpdates(botToken: String, updateId: Int): String {
+    val urlGetUpdates = "https://api.telegram.org/bot$botToken/getUpdates?offset=$updateId"
+    val client = HttpClient.newBuilder().build()
+    val request = HttpRequest.newBuilder().uri(URI.create(urlGetUpdates)).build()
+    val response: HttpResponse<String> = client.send(request, HttpResponse.BodyHandlers.ofString())
+    return response.body()
 }
